1fe964676f8b45add3c372b5f1a7424349392eae
Gildevan-Pereira <gildevanpereira.pro@gmail.com>
2022-04-26 23:02:34 -0300

    Criando o Pacote Entity e as Classes de Entidade

---
+++ src/main/java/com/serviceorder/api/entity/ServiceOrder.java
@@ -0,0 +1,10 @@
+package com.serviceorder.api.entity;
+
+import java.sql.Timestamp;
+
+public class ServiceOrder {
+	private long id;
+	private String uuid;
+	private Timestamp created_at;
+	private Timestamp removed_at;
+}
